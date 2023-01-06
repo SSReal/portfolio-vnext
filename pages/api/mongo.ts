8fcfd4d8ce6679bd9b0f17a920c59291d31534c2
Sajal Singhal <ss.13052001@outlook.com>
2023-01-06 18:59:46 +0530

    fix mongodb auth

--- pages/api/mongo.ts
+++ pages/api/mongo.ts
@@ -5,20 +5,20 @@ type Data = {
     message: string
 }
 
-const URI = `mongodb+srv://saja:${process.env.MONGO_PASSWORD}@portfolio-data-1.prqrq3y.mongodb.net/?retryWrites=true&w=majority`
+const URI = `mongodb+srv://sajal:${process.env.MONGO_PASSWORD}@portfolio-data-1.prqrq3y.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(URI);
 
 export default async function handler(
     req: NextApiRequest, 
     res: NextApiResponse<Data>,
 ) {
-    const c = await client.connect()
+    const c = await (client.connect()
     .catch((err) => {
         console.log(err);
         res.status(500).send({
             message: err.toString()
         })
-    })
+    }))
     console.log(c)
     client.close();
     res.status(200).send({message: "successful"});
