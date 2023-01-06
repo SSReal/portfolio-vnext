d46b36d7892bde1122c46732d7836b12f23ba7eb
Sajal Singhal <ss.13052001@outlook.com>
2023-01-06 18:39:08 +0530

    add mongodb test api

---
+++ pages/api/mongo.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {MongoClient, ServerApiVersion} from "mongodb";
+
+type Data = {
+    message: string
+}
+
+const URI = `mongodb+srv://sajal:${process.env.MONGO_PASSWORD}@portfolio-data-1.prqrq3y.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
+
+export default async function handler(
+    req: NextApiRequest, 
+    res: NextApiResponse<Data>,
+) {
+    const c = await client.connect()
+    .catch((err) => {
+        console.log(err);
+        res.status(500).send({
+            message: err.toString()
+        })
+    })
+    client.close();
+    res.status(200).send({message: "successful"});
+}
