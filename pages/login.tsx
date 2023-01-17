f05b12d089e56896e7069db38b7be4b1e72d314d
Sajal Singhal <ss.13052001@outlook.com>
2023-01-17 08:40:39 +0530

    UI tweaks

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { FaUserAlt } from "react-icons/fa";
+
+import loader from "../public/loader.gif";
 
 
 function Login() {
-    const [{username, password}, setRegDetails] = useState({username:"", password:""})
+    const [{ username, password }, setRegDetails] = useState({ username: "", password: "" })
     const [loggedInUser, setLoggedInUser] = useState("");
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         setLoggedInUser(localStorage.getItem('loggedInUser') || "")
@@ -18,25 +23,14 @@ function Login() {
         setLoggedInUser("");
     }
 
-    if(loggedInUser !== "") {
-        //a user is already logged in
-        //display logout button
-        return (
-            <div className = "flex flex-col items-center justify-center h-screen w-screen">
-                <p>Already logged in as {loggedInUser}</p>
-                <button onClick = {logout}>Log out</button>
-            </div>
-        )
-    }
-    
-    function changeHandler(e:any) {
-        if(e.target.name === "username") {
+    function changeHandler(e: any) {
+        if (e.target.name === "username") {
             setRegDetails({
                 username: e.target.value,
                 password,
             })
         }
-        else if(e.target.name === "password") {
+        else if (e.target.name === "password") {
             setRegDetails({
                 username,
                 password: e.target.value,
@@ -45,15 +39,16 @@ function Login() {
     }
 
     async function login() {
+        setLoading(true);
         const res = await fetch('/api/login', {
-            method: "POST", 
+            method: "POST",
             body: JSON.stringify({
-                username, 
+                username,
                 password
             })
         });
         const jsonRes = await res.json();
-        if(!jsonRes.success) {
+        if (!jsonRes.success) {
             alert("Login failed, check your details and try again")
             return;
         }
@@ -65,21 +60,39 @@ function Login() {
         alert(`Successfully logged in as ${username}`)
         await router.push('/edit');
 
+        setLoading(false);
         setRegDetails({
-            username:"",
-            password:"",
+            username: "",
+            password: "",
         })
+    }
+
+    if (loggedInUser !== "") {
+        //a user is already logged in
+        //display logout button
+        return (
+            <div className="flex flex-col items-center justify-center h-screen text-center mx-auto">
+                <FaUserAlt className = "text-9xl"/>
+                <p className = "text-5xl my-5">{loggedInUser}</p>
+                <button className = "mt-10" onClick={logout}>Log out</button>
+            </div>
+        )
     }
 
     return (
         <div className="flex flex-col h-screen w-screen justify-center">
+            {loading
+                && <div className="fixed bg-opacity-40 bg-slate-600 font-semibold w-screen h-screen flex items-center">
+                    <Image className="mx-auto opacity-60" src={loader} alt="loading" />
+                </div>
+            }
             <div className="rounded-lg border-black border-2 p-10 text-2xl grid grid-cols-2 max-w-xl mx-auto my-auto">
                 <label htmlFor="username">Username: </label>
-                <input name="username" type="text" value = {username} onChange = {changeHandler}/>
+                <input name="username" type="text" value={username} onChange={changeHandler} />
                 <label htmlFor="password">Password: </label>
-                <input name="password" type="password" value = {password} onChange = {changeHandler}/>
+                <input name="password" type="password" value={password} onChange={changeHandler} />
                 <button className="my-5 mx-5 col-start-2" onClick={login}>Login</button>
-                <Link href = "/register" className="hover:scale-105 hover:bg-opacity-10 hover:bg-gray-600 border-2 border-gray-200 rounded-lg text-center py-2 mx-5 col-start-2">Sign up</Link>
+                <Link href="/register" className="hover:scale-105 hover:bg-opacity-10 hover:bg-gray-600 border-2 border-gray-200 rounded-lg text-center py-2 mx-5 col-start-2">Sign up</Link>
             </div>
 
         </div>
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,27 +1,29 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useMemo, useState } from "react";
+import loader from "../public/loader.gif";
 
 
 function Register() {
-    const [{username, password, cpassword}, setRegDetails] = useState({username:"", password:"", cpassword:""})
-    
-    function changeHandler(e:any) {
-        if(e.target.name === "username") {
+    const [{ username, password, cpassword }, setRegDetails] = useState({ username: "", password: "", cpassword: "" })
+
+    function changeHandler(e: any) {
+        if (e.target.name === "username") {
             setRegDetails({
                 username: e.target.value,
                 password,
                 cpassword
             })
         }
-        else if(e.target.name === "password") {
+        else if (e.target.name === "password") {
             setRegDetails({
                 username,
                 password: e.target.value,
                 cpassword
             })
         }
-        else if(e.target.name === "cpassword") {
+        else if (e.target.name === "cpassword") {
             setRegDetails({
                 username,
                 password,
@@ -32,20 +34,21 @@ function Register() {
 
     const router = useRouter();
     async function regUser() {
+        setLoading(true);
 
         const res1 = await fetch('/api/find', {
-            method: "POST", 
-            body: JSON.stringify({username})
+            method: "POST",
+            body: JSON.stringify({ username })
         })
-        
-        const {found} = await res1.json();
 
-        if(found) {
+        const { found } = await res1.json();
+
+        if (found) {
             alert("The provided username already exists, please try another one");
             return;
         }
 
-        if(cpassword !== password) {
+        if (cpassword !== password) {
             alert("The passwords don't match, please check and try again");
             return;
         }
@@ -59,25 +62,34 @@ function Register() {
         })
 
         console.log(res2);
-        if(res2.status === 201) {
+        if (res2.status === 201) {
             alert("Registered successfully");
             await router.push('/login');
         }
-        setRegDetails({username:"", password:"", cpassword: ""})
+        setLoading(false);
+        setRegDetails({ username: "", password: "", cpassword: "" })
 
     }
 
+    const [loading, setLoading] = useState(false);
+
     return (
         <div className="flex flex-col h-screen w-screen justify-center">
+            {
+                loading &&
+                <div className="fixed bg-opacity-40 bg-slate-600 font-semibold w-screen h-screen flex items-center">
+                    <Image className="mx-auto opacity-60" src={loader} alt="loading" />
+                </div>
+            }
             <div className="rounded-lg border-black border-2 p-10 text-2xl grid grid-cols-2 max-w-xl mx-auto my-auto">
                 <label htmlFor="username">Username: </label>
-                <input name="username" type="text" value = {username} onChange = {changeHandler}/>
+                <input name="username" type="text" value={username} onChange={changeHandler} />
                 <label htmlFor="password">Password: </label>
-                <input name="password" type="password" value = {password} onChange = {changeHandler}/>
+                <input name="password" type="password" value={password} onChange={changeHandler} />
                 <label htmlFor="cpassword">Confirm Password: </label>
-                <input name="cpassword" type="password" value = {cpassword} onChange = {changeHandler}/>
+                <input name="cpassword" type="password" value={cpassword} onChange={changeHandler} />
                 <button className="my-5 mx-5 col-start-2" onClick={regUser}>Sign up</button>
-                <Link href = "/login" className="hover:scale-105 hover:bg-opacity-10 hover:bg-gray-600 border-2 border-gray-200 rounded-lg text-center py-2 mx-5 col-start-2">Login</Link>
+                <Link href="/login" className="hover:scale-105 hover:bg-opacity-10 hover:bg-gray-600 border-2 border-gray-200 rounded-lg text-center py-2 mx-5 col-start-2">Login</Link>
             </div>
 
         </div>
